f925201c17b0d3dc788977336620e9720bd51b9e
wenxiaoying <wenxiaoying@kaituocn.com>
2020-09-18 15:11:35 +0800

    初始化项目

---
+++ src/views/vantTest.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+
+<script>
+import { Button } from "vant";
+
+export default {
+  name: "vantTest",
+  components: {
+    [Button.name]: Button
+  }
+};
+</script>
